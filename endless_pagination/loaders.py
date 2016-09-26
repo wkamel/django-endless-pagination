f79528db78bc16efffdca2caf35d634748991c0e
Kamil <wkamel@users.noreply.github.com>
2016-09-26 18:32:57 +0200

    refreshed import_module source

--- endless_pagination/loaders.py
+++ endless_pagination/loaders.py
@@ -3,7 +3,8 @@
 from __future__ import unicode_literals
 
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.importlib import import_module
+# from django.utils.importlib import import_module
+from importlib import import_module
 
 
 def load_object(path):
